877f35c365386df1cf4f225af1fdf2e97c12e403
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-23 22:56:32 +0530

    v0.8

---
+++ src/icn/extension/WebContent/learns/action/LearnDocumentAction1.js
@@ -0,0 +1,31 @@
+define(["dojo/_base/declare","ecm/model/Action","dojo/_base/lang","ecm/widget/dialog/MessageDialog","dojo/_base/array"],
+ function(declare,Action,lang,MessageDialog,array) {
+    return declare("learns.action.LearnDocumentAction1",[Action],{
+
+        isVisible: function(repository, items, repositoryTypes, teamspace) {
+            return true;
+        },
+        isEnabled: function(repository, listType, items, teamspace, resultSet) {
+            return true;
+        },
+        performAction: function(repository, itemList, callback, teamspace, resultSet, parameterMap) {
+            var document = itemList[0]; // this is document
+            var string = "";
+            document.retrieveAttributes(lang.hitch(this,function(attributes) {
+                array.forEach(attributes, lang.hitch(this,function(attribute) {
+                    string += attribute.name +  " ";
+                }
+                this.show(string);
+            ));
+        },
+        show : function(attributes) {
+            var message = new MessageDialog({
+                text : attributes
+            });
+            message.show();
+        },
+        showInformation: function(data) {
+
+        }
+    });
+});
