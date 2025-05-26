e00730eae6220a000d649a182b079c6ca4849f34
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-26 22:04:33 +0530

    v0.9

--- src/icn/extension/WebContent/learns/widget/SearchDocuments.js
+++ src/icn/extension/WebContent/learns/widget/SearchDocuments.js
@@ -1,29 +1,16 @@
 define([
      "dojo/_base/declare",
      "ecm/widget/layout/_LaunchBarPane",
-     "ecm/widget/dialog/MessageDialog"
-     "ecm/model/Request"
-     "dojo/_base/lang",
-     "dojo/_base/connect",
-     "dojo/text!./templates/SearchDocuments.html",
-     "dijit/layout/BorderContainer",
-     "dijit/layout/ContentPane",
-     "dijit/form/TextBox",
-     "dijit/form/Button",
-     "gridx/Grid",
-     "dojo/data/ItemFileWriteStore",
-     "gridx/core/model/cache/Sync",
-     "gridx/modules/ColumnResizer"
-] , function(declare,_LaunchBarPane , MessageDialog, Request,  lang , connect, template) {
+     "dojo/text!./templates/SearchDocuments.html"
+] , function(declare,_LaunchBarPane,template) {
     return declare("learns.widget.SearchDocuments",[ _LaunchBarPane ] ,{
-
         templateString : template,
-
         widgetsInTemplate : true,
 
         postCreate : function() {
             this.inherited(arguments);
         },
+
         loadContent: function() {
             if (!this.isLoaded) {
                 this.isLoaded =  true;
