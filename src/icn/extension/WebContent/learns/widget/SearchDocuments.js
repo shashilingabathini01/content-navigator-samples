9485d9fb55c1b3e511e867a7a48b9cf8f1fecd13
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-26 22:18:49 +0530

    v0.9

--- src/icn/extension/WebContent/learns/widget/SearchDocuments.js
+++ src/icn/extension/WebContent/learns/widget/SearchDocuments.js
@@ -2,7 +2,9 @@ define([
      "dojo/_base/declare",
      "ecm/widget/layout/_LaunchBarPane",
      "dojo/text!./templates/SearchDocuments.html",
-     "dijit/layout/BorderContainer"
+     "dijit/layout/BorderContainer",
+     "dijit/layout/ContentPane",
+     "dijit/form/TextBox"
 ] , function(declare,_LaunchBarPane,template) {
     return declare("learns.widget.SearchDocuments",[ _LaunchBarPane ] ,{
         templateString : template,
