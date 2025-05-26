2be72ab1fa84568548c75d78355bd67e84e41854
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-26 22:38:47 +0530

    v0.9

--- src/icn/extension/WebContent/learns/widget/SearchDocuments.js
+++ src/icn/extension/WebContent/learns/widget/SearchDocuments.js
@@ -18,28 +18,34 @@ define([
         widgetsInTemplate : true,
         postCreate : function() {
             this.inherited(arguments);
-            this._setEmptyGrid();
+           // this._setEmptyGrid();
         },
-        _setEmptyGrid : function() {
-            var gridData = {
-                    identifier: 'id',
-                    items: []
-            };
-            var store = new ItemFileWriteStore({
-                data: gridData
-            });
-            var COLUMNS = [];
-            this._setGridStructure(COLUMNS);
-            this.documents.setStore(store);
-        },
-        _setGridStructure : function(columns) {
-            var gridLayout = [];
-            var colWidth = "120px";
-            for(var x = 0 ; x < columns.length ; x++) {
-                gridLayout.push({id:x + 1 ,field:columns[x].value,name:columns[x].displayName,width:colWidth});
+//        _setEmptyGrid : function() {
+//            var gridData = {
+//                    identifier: 'id',
+//                    items: []
+//            };
+//            var store = new ItemFileWriteStore({
+//                data: gridData
+//            });
+//            var COLUMNS = [];
+//            this._setGridStructure(COLUMNS);
+//            this.documents.setStore(store);
+//        },
+        loadContent: function() {
+            if (!this.isLoaded) {
+                this.isLoaded =  true;
+                this.needReset = false;
             }
-            console.dir(gridLayout)
-            this.documents.setColumns(gridLayout);
         }
+//        _setGridStructure : function(columns) {
+//            var gridLayout = [];
+//            var colWidth = "120px";
+//            for(var x = 0 ; x < columns.length ; x++) {
+//                gridLayout.push({id:x + 1 ,field:columns[x].value,name:columns[x].displayName,width:colWidth});
+//            }
+//            console.dir(gridLayout)
+//            this.documents.setColumns(gridLayout);
+//        }
     });
 });
