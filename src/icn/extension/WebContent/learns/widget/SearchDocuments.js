b6888b80cb6ce01b0fbab4a0c435991cd796a60e
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-26 22:35:15 +0530

    v0.9

--- src/icn/extension/WebContent/learns/widget/SearchDocuments.js
+++ src/icn/extension/WebContent/learns/widget/SearchDocuments.js
@@ -32,7 +32,7 @@ define([
             this._setGridStructure(COLUMNS);
             this.documents.setStore(store);
         },
-        _setGridStructure : function() {
+        _setGridStructure : function(columns) {
             var gridLayout = [];
             var colWidth = "120px";
             for(var x = 0 ; x < columns.length ; x++) {
@@ -40,78 +40,6 @@ define([
             }
             console.dir(gridLayout)
             this.documents.setColumns(gridLayout);
-        },
-
-        loadContent: function() {
-            if (!this.isLoaded) {
-                this.isLoaded =  true;
-                this.needReset = false;
-            }
-        },
-        _setGridStore : function() {
-            var gridData = {
-                    identifier: 'id',
-                    items: []
-                };
-            var _items = [];
-            for(var x = 0; x < rows.length ; x++) {
-                var rowData = {};
-                Object.keys(rows[x]).forEach(function(key) {
-                      rowData[key] = rows[x][key];
-                });
-                rowData["id"] = x + 1;
-                _items.push(rowData);
-            }
-            gridData.items = _items;
-            var store = new ItemFileWriteStore({
-                data: gridData
-            });
-            console.dir(store)
-            this.documents.setStore(store);
-            var _this = this;
-            connect.connect(this.documents, "onRowClick", this, lang.hitch(this,function(event){
-                var selectedRowIndex = event.rowId;
-                var selectRowData = this.documents.model.byId(selectedRowIndex);
-                // set field data here
-                console.log(selectRowData);
-                // document Id and then open the document
-                var actions  = ecm.model.desktop.getActionHandler();
-                actions["open"](selectRowData.documentId);
-            }));
-            this.documents.startup();
-            this.documents.resize();
-        },
-        searchDocuments : function() {
-            if (this.searchText.value() == "") {
-                // pop up
-                var messageDialog = new MessageDialog({
-                    text : 'search text can not be empty'
-                });
-                return;
-            }
-            var requestParams = {};
-            requestParams['searchText'] = this.searchText.value();
-            Request.invokePluginService("ICNLearns","SearchDocuments", {
-                requestParams : requestParams,
-                requestCompleteCallback : lang.hitch(this,function(data) {
-                    console.dir(data);
-                    if(data.error == null && data.status === "success") {
-                        this._setGridStructure(data.results.columns);
-                        this._setGridStore(data.results.rows);
-                    } else {
-                        var message =  new MessageDialog({
-                            text : 'An error while fetching details'
-                        });
-                        message.show();
-                    }
-                }),
-                requestFailedCallback :lang.hitch(this,function(data) {
-                    var message =  new MessageDialog({
-                        text : 'An error while fetching details'
-                    });
-                    message.show();
-                })
-            });
         }
     });
 });
