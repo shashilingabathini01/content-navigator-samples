c84913fde4c718ce92254fdc2e197539b17bcb66
shashilingabathini <shashi.lingabathini@gmail.com>
2025-05-27 16:51:20 +0530

    v0.9

--- src/icn/extension/WebContent/learns/widget/SearchDocuments.js
+++ src/icn/extension/WebContent/learns/widget/SearchDocuments.js
@@ -10,41 +10,20 @@ define([
      "dijit/layout/BorderContainer",
      "dijit/layout/ContentPane",
      "dijit/form/TextBox",
-     "dijit/form/Button"
+     "dijit/form/Button",
+     "gridx/Grid"
 ] , function(declare,_LaunchBarPane,ItemFileWriteStore,MessageDialog,Request,lang,connect,template) {
     return declare("learns.widget.SearchDocuments",[ _LaunchBarPane ] ,{
         templateString : template,
         widgetsInTemplate : true,
         postCreate : function() {
             this.inherited(arguments);
-           // this._setEmptyGrid();
         },
-//        _setEmptyGrid : function() {
-//            var gridData = {
-//                    identifier: 'id',
-//                    items: []
-//            };
-//            var store = new ItemFileWriteStore({
-//                data: gridData
-//            });
-//            var COLUMNS = [];
-//            this._setGridStructure(COLUMNS);
-//            this.documents.setStore(store);
-//        },
         loadContent: function() {
             if (!this.isLoaded) {
                 this.isLoaded =  true;
                 this.needReset = false;
             }
         }
-//        _setGridStructure : function(columns) {
-//            var gridLayout = [];
-//            var colWidth = "120px";
-//            for(var x = 0 ; x < columns.length ; x++) {
-//                gridLayout.push({id:x + 1 ,field:columns[x].value,name:columns[x].displayName,width:colWidth});
-//            }
-//            console.dir(gridLayout)
-//            this.documents.setColumns(gridLayout);
-//        }
     });
 });
